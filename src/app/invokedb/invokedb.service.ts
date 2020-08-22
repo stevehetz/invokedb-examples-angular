af91fd662d0245069703e5fc5d363df83842f1f2
Steve Hetzel <stevehetzel2@gmail.com>
2020-08-22 09:34:42 -0400

    send key as bearer

--- src/app/invokedb/invokedb.service.ts
+++ src/app/invokedb/invokedb.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BASE_URL, API_KEY } from 'src/invoke-config.json';
 import { IGetParams } from './invokedb.params';
 
@@ -9,11 +9,15 @@ import { IGetParams } from './invokedb.params';
 export class InvokedbService {
   constructor(private http: HttpClient) {}
 
+  getHeaders() {
+    return new HttpHeaders().set('Authorization', `Bearer ${API_KEY}`);
+  }
+
   get(tableName: string, params: IGetParams, filter?: any) {
     const { skip, limit, sort } = params;
 
-    let urlQuery = `?api_key=${API_KEY}`;
-    urlQuery += `&skip=${skip}`;
+    let urlQuery = '';
+    urlQuery += `?skip=${skip}`;
     urlQuery += `&limit=${limit}`;
 
     if (sort) {
@@ -21,23 +25,28 @@ export class InvokedbService {
       urlQuery += `&sort_dir=${sort.sortDir}`;
     }
 
+    const headers = this.getHeaders();
+
     return filter
-      ? this.http.post(`${BASE_URL}/search/${tableName}${urlQuery}`, filter)
-      : this.http.get(`${BASE_URL}/get/${tableName}${urlQuery}`);
+      ? this.http.post(`${BASE_URL}/search/${tableName}${urlQuery}`, filter, { headers })
+      : this.http.get(`${BASE_URL}/get/${tableName}${urlQuery}`, { headers });
   }
 
   update(tableName: string, data: any) {
-    const url = `${BASE_URL}/update/${tableName}?api_key=${API_KEY}`;
-    return this.http.put(url, data);
+    const headers = this.getHeaders();
+    const url = `${BASE_URL}/update/${tableName}`;
+    return this.http.put(url, data, { headers });
   }
 
   delete(tableName: string, data: any) {
-    const url = `${BASE_URL}/delete/${tableName}?api_key=${API_KEY}`;
-    return this.http.post(url, data);
+    const headers = this.getHeaders();
+    const url = `${BASE_URL}/delete/${tableName}`;
+    return this.http.post(url, data, { headers });
   }
 
   create(tableName: string, data: any) {
-    const url = `${BASE_URL}/create/${tableName}?api_key=${API_KEY}`;
-    return this.http.post(url, data);
+    const headers = this.getHeaders();
+    const url = `${BASE_URL}/create/${tableName}`;
+    return this.http.post(url, data, { headers });
   }
 }
