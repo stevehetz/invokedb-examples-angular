845d233ed0f758d1a41936fc946b76e3dd4ff5d7
Steve Hetzel <stevehetzel2@gmail.com>
2020-09-07 15:06:10 -0400

    invokedb client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InvokeDBClient } from 'invokedb';
+import { API_KEY } from 'src/invoke-config.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  ngOnInit() {}
+  async ngOnInit() {
+    const invokedb = new InvokeDBClient({ apiKey: API_KEY });
+    const contacts = invokedb.table('contacts');
+
+    const res = await contacts.get({
+      skip: 0,
+      limit: 10,
+      filter: {
+        first_name: {
+          value: 'Low',
+          type: 'contains',
+          case: 'insensitive'
+        }
+      }
+    });
+
+    console.log(res);
+  }
 }
