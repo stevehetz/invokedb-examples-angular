cc10522a6fca019172778e275c8798a88199c4e5
Steve Hetzel <stevehetzel2@gmail.com>
2020-09-08 15:33:15 -0400

    remove invokedb-node example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,5 @@ import { API_KEY } from 'src/invoke-config.json';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  async ngOnInit() {
-    const invokedb = new InvokeDBClient({ apiKey: API_KEY });
-    const contacts = invokedb.table('contacts');
-
-    const res = await contacts.get({
-      skip: 0,
-      limit: 10,
-      filter: {
-        first_name: {
-          value: 'Low',
-          type: 'contains',
-          case: 'insensitive'
-        }
-      }
-    });
-
-    console.log(res);
-  }
+  async ngOnInit() {}
 }
