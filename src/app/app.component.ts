45e04d6ef2525b892923a9dee499e0e6d58b3685
Steve Hetzel <stevehetzel2@gmail.com>
2020-09-16 09:35:38 -0400

    invokedb-node

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent implements OnInit {
     const winereview = invokedb.table('winereview');
 
     const res = await contacts.count();
-    console.log(res);
 
     const res2 = await contacts
       .find({
@@ -30,12 +29,23 @@ export class AppComponent implements OnInit {
 
     const res3 = await contacts.findOne({
       first_name: {
-        $ctn: 'low',
+        $ctn: 'heather',
         case: 'i'
       }
     });
 
     console.log(res3);
+
+    if (res3) {
+      /*await contacts.update([
+        {
+          _id: res3._id,
+          first_name: 'abcdefg'
+        }
+      ]);*/
+
+      await contacts.delete(7);
+    }
 
     const res4 = await contacts
       .find({ first_name: 'Lowrance' })
@@ -44,5 +54,16 @@ export class AppComponent implements OnInit {
       .exec();
 
     console.log(res4.data);
+
+    /*await contacts.insert([
+      {
+        first_name: 'John',
+        last_name: 'Stamos'
+      },
+      {
+        first_name: 'Lindsey',
+        last_name: 'Hetzel'
+      }
+    ]);*/
   }
 }
